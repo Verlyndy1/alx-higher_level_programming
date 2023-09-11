2a3fb0443806d139a91ea6b204522e820451714c
root <ezenwanneoluchukwu@yahoo.com>
2023-09-11 10:26:38 -0700

    java

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,18 +1,7 @@
 #!/usr/bin/node
-
+const a = parseInt(process.argv[2]);
+const b = parseInt(process.argv[3]);
 function add (a, b) {
-  const sum = a + b;
-  console.log(sum);
+  return (a + b);
 }
-
-const arg1 = process.argv[2];
-const arg2 = process.argv[3];
-
-const num1 = parseInt(arg1);
-const num2 = parseInt(arg2);
-
-if (!isNaN(num1) && !isNaN(num2)) {
-  add(num1, num2);
-} else {
-  console.log('Invalid arguments');
-}
+console.log(add(a, b));
